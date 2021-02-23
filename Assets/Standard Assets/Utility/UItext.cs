18aebc9ce718bb59a9c2d47e38120d56a792665b
ariandriussi <arian.driussi.dev@gmail.com>
2021-02-23 18:27:56 -0300

    Gitignore fix final

---
+++ Assets/Standard Assets/Utility/UItext.cs
@@ -0,0 +1,19 @@
+﻿namespace UnityStandardAssets.Utility
+{
+    public class UItext
+    {
+        private string uItext;
+
+        public string GetUItext()
+        {
+            return uItext;
+        }
+
+        internal void SetUItext(string value)
+        {
+            uItext = value;
+        }
+
+        internal string text;
+    }
+}
